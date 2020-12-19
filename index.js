0c33f36c6b17cdf4edcd6690653a9252c2f73169
sabyrd39 <48448793+sabyrd39@users.noreply.github.com>
2020-12-19 17:54:33 -0500

    No changes - Heroku throwing an error and had to fix

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ var loggedInID = 0;
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
-    next(););
-}
+    next()
+});
+
 
 app.use(express.json());
 
