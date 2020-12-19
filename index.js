cd867785b113778b6b2bd8a09a6be9554fe9b069
sabyrd39 <48448793+sabyrd39@users.noreply.github.com>
2020-12-19 16:01:57 -0500

    No changes - Heroku throwing an error and had to fix it

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ const verifyJWT = (req, res, next) => {
 
 app.use(express.json());
 
-/*const path = require('path');
+const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/src')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'App.js'));
   });
-}*/
+}
 
 const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://webdev4166final.herokuapp.com/'];
 const corsOptions = {
