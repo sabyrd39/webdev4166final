2b192942acdd823b37b8df5923869c8ba504d888
sabyrd39 <48448793+sabyrd39@users.noreply.github.com>
2020-12-19 16:48:07 -0500

    No changes - Heroku throwing an error and had to fix it

--- index.js
+++ index.js
@@ -245,8 +245,8 @@ app.get('/dashboard', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT = 3001;
-app.listen(process.env.PORT, () => {
+const PORT = 3001;
+app.listen(process.env.PORT || PORT, () => {
     console.log(`TRUE port: ${process.env.PORT}`);
     console.log(`Running on the following port: ${PORT}`);
 });
