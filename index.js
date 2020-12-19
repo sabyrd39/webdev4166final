e716edaaca0551fce6e82a18cf77d801b1604317
sabyrd39 <48448793+sabyrd39@users.noreply.github.com>
2020-12-19 18:45:36 -0500

    No changes - Heroku throwing an error and had to fix

--- index.js
+++ index.js
@@ -39,12 +39,6 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-/*app.use(cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"],
-    credentials: true,
-}));*/
-
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
